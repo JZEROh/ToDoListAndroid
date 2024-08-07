5ffa0ae31000bac85fc6f8055b4e954646bc6b3a
kaka <parkhk981116@gmail.com>
2024-08-07 11:23:31 +0900

    login and jwt

--- app/src/main/java/com/example/todoproject/FirstpageActivity.kt
+++ app/src/main/java/com/example/todoproject/FirstpageActivity.kt
@@ -1,14 +1,29 @@
 package com.example.todoproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.todoproject.databinding.ActivityFirstpageBinding
 
 
 class FirstpageActivity : AppCompatActivity() {
-
+lateinit var binding: ActivityFirstpageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_firstpage)
+        binding = ActivityFirstpageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.moveLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.moveJoin.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
+
 
 
     }
