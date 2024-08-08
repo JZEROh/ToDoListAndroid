384fa1fd0bf382ba24d209736c5eb13267869a47
Jo <mz8152@naver.com>
2024-08-08 14:33:53 +0900

    WeatherFunction

--- app/src/main/java/com/example/todoproject/RecyclerViewMain.kt
+++ app/src/main/java/com/example/todoproject/RecyclerViewMain.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
+import com.example.todoproject.adapter.ViewPagerAdapter
+import com.example.todoproject.data.ToDoItem
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
